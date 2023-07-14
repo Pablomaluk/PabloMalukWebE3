847a963f87702926ae19171db9255dfc04d1bca9
Pablomaluk <pablo.maluk@uc.cl>
2023-07-13 21:44:49 -0400

    Update Routing.jsx

--- Frontend/src/Routing.jsx
+++ Frontend/src/Routing.jsx
@@ -1,13 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Instructions from "./backup/instructions/instructions"
-import Attack from "./backup/instructions/rules/attack"
-import Building from "./backup/instructions/rules/building"
-import Movement from "./backup/instructions/rules/movement"
-import Review from "./backup/instructions/rules/review"
-import Rules from "./backup/instructions/rules/rules"
-import Training from "./backup/instructions/rules/training"
 import Login from  "./login/Login"
-import About from "./about/About"
 import Game from "./game/Game"
 import Register from "./register/Register"
 import Lobby from "./lobby/Lobby"
@@ -21,14 +13,6 @@ function Routing() {
                 <Route path={'/register'} element={<Register/>}/>
                 <Route path={'/lobby'} element={<Lobby/>}/>
                 <Route path={'/game'} element={<Game/>}/>
-                <Route path={'/about'} element={<About/>}/>
-                <Route path={'/instructions'} element={<Instructions/>}/>
-                <Route path={'/rules'} element={<Rules/>}/>
-                <Route path={'/rules/1'} element={<Review/>}/>
-                <Route path={'/rules/2'} element={<Building/>}/>
-                <Route path={'/rules/3'} element={<Training/>}/>
-                <Route path={'/rules/4'} element={<Movement/>}/>
-                <Route path={'/rules/5'} element={<Attack/>}/>
                 <Route path={'/'} element={<Login/>} />
             </Routes>
         </BrowserRouter>
